204312ce40063fc0474a44b56dacbf71304a7da3
Obukwelu Ebuka Valentine <obukweluebuka@gmail.com>
2021-10-21 19:46:41 +0200

    setup phaser in ionic application

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import Phaser from 'phaser';
+import MainScene from 'src/scenes/main-scene';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  phaserGame: Phaser.Game;
+  config: Phaser.Types.Core.GameConfig;
+  constructor() {
+    this.config = {
+      type: Phaser.AUTO,
+      height: 600,
+      width: 800,
+      scene: [MainScene],
+      parent: 'container',
+      physics: {
+        default: 'arcade',
+        arcade: {
+          gravity: { y: 100 },
+        },
+      },
+    };
+  }
 
-  constructor() {}
-
+  ngOnInit(): void {
+    this.phaserGame = new Phaser.Game(this.config);
+  }
 }
---
+++ src/scenes/main-scene.ts
@@ -0,0 +1,16 @@
+import Phaser from 'phaser';
+
+export default class MainScene extends Phaser.Scene {
+  constructor() {
+    super({ key: 'main' });
+  }
+  create() {
+    console.log('create method');
+  }
+  preload() {
+    console.log('preload method');
+  }
+  update() {
+    console.log('update method');
+  }
+}
